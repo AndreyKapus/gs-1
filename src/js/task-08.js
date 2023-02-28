1091c3f33da023191b099e4b8a413b307f18d036
AndreyKapus <kapustnikov@ukr.net>
2023-02-28 17:51:42 +0200

    8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,15 @@
+const formEl = document.querySelector('.login-form');
 
+const onFormSubmit = (e) => {
+    e.preventDefault();
+    const {elements: {email, password}} = e.currentTarget;
+
+    if (email.value === '' || password.value === '') {
+        alert("заполни поле")
+    };
+    const object = {email: email.value, password: password.value};
+    console.log(object);
+    e.currentTarget.reset()
+}
+
+formEl.addEventListener('submit', onFormSubmit)
