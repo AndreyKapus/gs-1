9d56e367cbba68e99ac8e19b5188a8bc0f05003e
AndreyKapus <kapustnikov@ukr.net>
2023-02-28 19:08:19 +0200

    9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,17 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColorBtnEl = document.querySelector('.change-color');
+const bodyEl = document.querySelector('body');
+const textEl = document.querySelector('.color')
+
+const handleBackgroundColorChange = (e) => {
+  e.preventDefault();
+  let bgColor = bodyEl.style.backgroundColor
+  bgColor = getRandomHexColor();
+  textEl.textContent = bgColor
+
+}
+
+changeColorBtnEl.addEventListener('click', handleBackgroundColorChange)
--- src/js/task-10.js
+++ src/js/task-10.js
@@ -3,3 +3,19 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const inputEl = document.querySelector('input');
+const btnCreate = document.querySelector('[data-create]');
+const btnDestroy = document.querySelector('[data-destroy]');
+const boxesEl = document.getElementById('boxes')
+
+function createBoxes(amount) {
+
+  let divchick = document.createElement('div');
+  let elements = divchick += amount;
+  console.log(divchick)
+  boxesEl.insertAdjacentHTML('afterbegin', elements)
+}
+
+const inp = inputEl.addEventListener('input', createBoxes)
+btnCreate.addEventListener('click', createBoxes)
