2464fb4fdcb010ad06af1ac4cb762f4846e89b0b
AndreyKapus <kapustnikov@ukr.net>
2023-02-28 16:05:32 +0200

    7done

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -1,3 +1,8 @@
 const inputEl = document.getElementById('font-size-control');
 const textEl = document.getElementById('text');
 
+const fontSize = (e) => {
+    textEl.style.fontSize = `${e.target.value}px`
+}
+
+inputEl.addEventListener("input", fontSize)
